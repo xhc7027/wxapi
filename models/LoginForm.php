83d4202cdb55e2f633d1255311b2c4ac221a54f6
terrence <tianmingxing@idouzi.com>
2017-11-16 14:57:01 +0800

    #issue-#1931 修复后台验证码刷新多次导致登录不进去的问题

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -3,7 +3,6 @@
 namespace app\models;
 
 use Yii;
-use yii\base\InvalidConfigException;
 use yii\base\Model;
 
 /**
@@ -16,7 +15,7 @@ class LoginForm extends Model
 {
     public $username;
     public $password;
-    public $verificationCode;
+    public $captcha;
     public $rememberMe = true;
 
     private $_user = false;
@@ -29,47 +28,17 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password', 'verificationCode'], 'required'],
+            [['username', 'password', 'captcha'], 'required'],
             // rememberMe must be a boolean value
             ['rememberMe', 'boolean'],
             // password is validated by validatePassword()
             ['password', 'validatePassword'],
-            ['verificationCode', 'string', 'min' => 6, 'max' => 7],
-            ['verificationCode', 'verificationCode'],
+            ['captcha', 'captcha'],
         ];
     }
 
     /**
-     * 验证用户输入的验证码
-     *
-     * @param $attribute
-     * @param $params
-     * @throws InvalidConfigException
-     */
-    public function verificationCode($attribute, $params)
-    {
-        if (!$this->hasErrors()) {
-            $captcha = Yii::$app->createController('common/captcha');
-            if ($captcha === false) {
-                throw new InvalidConfigException('Invalid CAPTCHA action ID: captcha');
-            }
-
-            /* @var $controller \yii\base\Controller */
-            list($controller, $actionID) = $captcha;
-            $action = $controller->createAction($actionID);
-            if ($action === null) {
-                throw new InvalidConfigException('Invalid CAPTCHA action ID: captcha');
-            }
-
-            if (!$action->validate($this->verificationCode, false)) {
-                $this->addError($attribute, '验证码错误。');
-            }
-        }
-    }
-
-    /**
-     * Validates the password.
-     * This method serves as the inline validation for password.
+     * Validates the password* This method serves as the inline validation for password.
      *
      * @param string $attribute the attribute currently being validated
      * @param array $params the additional name-value pairs given in the rule
@@ -119,9 +88,8 @@ class LoginForm extends Model
         return [
             'username' => '用户名',
             'password' => '密码',
+            'captcha' => '验证码',
             'rememberMe' => '记住我',
-            'verificationCode' => '验证码',
         ];
     }
 }
-
